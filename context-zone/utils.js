b0a0831bc42a0923c837753383c46624e46bfb21
kensan914 <52157596+kensan914@users.noreply.github.com>
2021-05-24 13:26:34 +0900

    add comments to main.js

--- context-zone/utils.js
+++ context-zone/utils.js
@@ -1,7 +1,10 @@
 import "zone.js";
-import { currentLayers, LayerStack, resetLayerStack } from "../ContextJS/src/Layers.js";
+import {
+  currentLayers,
+  LayerStack,
+  resetLayerStack,
+} from "../ContextJS/src/Layers.js";
 import { withLayersZone } from "./contextZone.js";
-
 
 /**
  * return [restoreLayerStack, unrestoreLayerStack]
@@ -13,29 +16,29 @@ export const useReplayLayerStack = (frame, zoneName) => {
 
   // LayerStackから_zoneNameのframeを削除
   const deleteFromLayerStack = (_zoneName) => {
-    const targetIndex = LayerStack.findIndex(elm => (
-      elm.zoneName === _zoneName
-    ));
+    const targetIndex = LayerStack.findIndex(
+      (elm) => elm.zoneName === _zoneName
+    );
     if (targetIndex !== -1) {
       LayerStack.splice(targetIndex, 1);
     }
-  }
+  };
 
   const restoreLayerStack = () => {
     applyToLayerStack(zonedLayerStack);
   };
   const unrestoreLayerStack = () => {
-    zonedLayerStack.forEach(_frame => {
+    zonedLayerStack.forEach((_frame) => {
       if (_frame.zoneName) {
         deleteFromLayerStack(_frame.zoneName);
       } else {
       }
     });
-    console.error([...LayerStack]);
+    // console.error([...LayerStack]);
   };
 
   return [restoreLayerStack, unrestoreLayerStack];
-}
+};
 
 /**
  * return [replayZoneCurrentEnter, replayZoneCurrentLeave]
@@ -48,17 +51,23 @@ export const useReplayZoneCurrent = (zone, rootZone) => {
       configurable: true,
       enumerable: false,
     });
-  }
+  };
 
   const replayZoneCurrentEnter = () => replayZoneCurrent(zone);
   const replayZoneCurrentLeave = () => replayZoneCurrent(rootZone);
 
   return [replayZoneCurrentEnter, replayZoneCurrentLeave];
-}
+};
 
-
-export const generateOnInvokeTaskCallback = (frame, zoneName, wrapWithFrameZone) => {
-  const [restoreLayerStack, unrestoreLayerStack] = useReplayLayerStack(frame, zoneName);
+export const generateOnInvokeTaskCallback = (
+  frame,
+  zoneName,
+  wrapWithFrameZone
+) => {
+  const [restoreLayerStack, unrestoreLayerStack] = useReplayLayerStack(
+    frame,
+    zoneName
+  );
 
   return (delegate, curr, target, task, applyThis, applyArgs) => {
     let _task = task;
@@ -67,16 +76,20 @@ export const generateOnInvokeTaskCallback = (frame, zoneName, wrapWithFrameZone)
         task,
         restoreLayerStack,
         unrestoreLayerStack,
-        wrapWithFrameZone,
+        wrapWithFrameZone
       );
     }
 
     return delegate.invokeTask(target, _task, applyThis, applyArgs);
-  }
-}
+  };
+};
 
-
-const wrapCallbackTask = (task, invokeTaskCallback, endTaskCallback, wrapWithFrameZone) => {
+const wrapCallbackTask = (
+  task,
+  invokeTaskCallback,
+  endTaskCallback,
+  wrapWithFrameZone
+) => {
   if (!task.isWrappedCallback) {
     // not wrapped yet
     const _callback = task.callback;
@@ -90,16 +103,15 @@ const wrapCallbackTask = (task, invokeTaskCallback, endTaskCallback, wrapWithFra
       //   _callback.apply(this, arguments);
       // });
       // endTaskCallback();
-    }
+    };
     task.isWrappedCallback = true;
   } else {
     // already wrapped
-    console.log("already wrapped.");
+    // console.log("already wrapped.");
   }
 
   return task;
-}
-
+};
 
 /**
  * zone nameの生成
@@ -128,7 +140,11 @@ export function applyToLayerStack(from) {
   const maxLengthCommonAncestry = Math.min(fromLength, LayerStackLength);
   let commonAncestryLength = 0;
 
-  while (commonAncestryLength < maxLengthCommonAncestry && from[commonAncestryLength]?.zoneName === LayerStack[commonAncestryLength]?.zoneName) {
+  while (
+    commonAncestryLength < maxLengthCommonAncestry &&
+    from[commonAncestryLength]?.zoneName ===
+      LayerStack[commonAncestryLength]?.zoneName
+  ) {
     commonAncestryLength++;
   }
 
@@ -149,13 +165,15 @@ export function popFrame() {
   const afterPop = currentLayers();
 
   // #TODO: we should probably .reverse() the list to deactivate the last activated layer first
-  withLayers && withLayers
-    .filter(l => beforePop.includes(l) && !afterPop.includes(l))
-    .forEach(l => l._emitDeactivateCallbacks());
+  withLayers &&
+    withLayers
+      .filter((l) => beforePop.includes(l) && !afterPop.includes(l))
+      .forEach((l) => l._emitDeactivateCallbacks());
 
-  withoutLayers && withoutLayers
-    .filter(l => !beforePop.includes(l) && afterPop.includes(l))
-    .forEach(l => l._emitActivateCallbacks());
+  withoutLayers &&
+    withoutLayers
+      .filter((l) => !beforePop.includes(l) && afterPop.includes(l))
+      .forEach((l) => l._emitActivateCallbacks());
 }
 
 export function pushFrame(frame) {
@@ -165,13 +183,15 @@ export function pushFrame(frame) {
 
   LayerStack.push(frame);
 
-  withLayers && withLayers
-    .filter(l => !beforePush.includes(l))
-    .forEach(l => l._emitActivateCallbacks());
+  withLayers &&
+    withLayers
+      .filter((l) => !beforePush.includes(l))
+      .forEach((l) => l._emitActivateCallbacks());
 
-  withoutLayers && withoutLayers
-    .filter(l => beforePush.includes(l))
-    .forEach(l => l._emitDeactivateCallbacks());
+  withoutLayers &&
+    withoutLayers
+      .filter((l) => beforePush.includes(l))
+      .forEach((l) => l._emitDeactivateCallbacks());
 }
 
 export function frameEquals(frame1, frame2) {
@@ -180,21 +200,28 @@ export function frameEquals(frame1, frame2) {
   // all props are StrictEqual, except withLayers and withoutLayers
   const shallowCompare = (obj1, obj2) =>
     Object.keys(obj1).length === Object.keys(obj2).length &&
-    Object.keys(obj1).every(key => {
+    Object.keys(obj1).every((key) => {
       if (layerListProperties.includes(key)) {
         return true; // checked later
       }
-      return obj2.hasOwnProperty(key) && obj1[key] === obj2[key]
+      return obj2.hasOwnProperty(key) && obj1[key] === obj2[key];
     });
 
-  if (!shallowCompare(frame1, frame2)) { return false; }
+  if (!shallowCompare(frame1, frame2)) {
+    return false;
+  }
 
   // withLayers and withoutLayers should contain the same layers in order
-  return layerListProperties.every(prop => {
+  return layerListProperties.every((prop) => {
     const arr1 = frame1[prop];
     const arr2 = frame2[prop];
-    if (arr1 && arr2) { // both have prop set
-      if (!Array.isArray(arr1) || !Array.isArray(arr2) || arr1.length !== arr2.length) {
+    if (arr1 && arr2) {
+      // both have prop set
+      if (
+        !Array.isArray(arr1) ||
+        !Array.isArray(arr2) ||
+        arr1.length !== arr2.length
+      ) {
         return false;
       }
       return arr1.every((layer, index) => layer === arr2[index]);
@@ -203,7 +230,6 @@ export function frameEquals(frame1, frame2) {
     return !arr1 && !arr2; // both do not define the prop is fine, too
   });
 }
-
 
 export function getCurrentLayerStack() {
   if (LayerStack.length === 0) {
@@ -214,10 +240,10 @@ export function getCurrentLayerStack() {
 
     // use copied arrays of layers
     if (frame.withLayers) {
-      resultFrame.withLayers = Array.from(frame.withLayers)
+      resultFrame.withLayers = Array.from(frame.withLayers);
     }
     if (frame.withoutLayers) {
-      resultFrame.withoutLayers = Array.from(frame.withoutLayers)
+      resultFrame.withoutLayers = Array.from(frame.withoutLayers);
     }
 
     return Object.assign(resultFrame, frame);
